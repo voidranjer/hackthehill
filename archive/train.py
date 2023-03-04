c2acf49bb613c6ebc74872878bbf8590ccaf5954
meekranjer <logicism81@gmail.com>
2023-03-04 05:48:32 -0500

    Starting fresh

--- archive/train.py
+++ archive/train.py
@@ -1,26 +1,41 @@
-# set the matplotlib backend so figures can be saved in the background
-import os
-import cv2
-import pickle
-import argparse
-import numpy as np
-import matplotlib.pyplot as plt
-from imutils import paths
-from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelBinarizer
-from tensorflow.keras.optimizers import SGD
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Dropout
-from tensorflow.keras.applications import ResNet50
-from tensorflow.keras.layers import AveragePooling2D
+# import the necessary packages
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import SGD
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import pickle
+import cv2
+import os
+
+# # set the matplotlib backend so figures can be saved in the background
 import matplotlib
 matplotlib.use("Agg")
-# import the necessary packages
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+                help="path to input dataset")
+ap.add_argument("-m", "--model", required=True,
+                help="path to output serialized model")
+ap.add_argument("-l", "--label-bin", required=True,
+                help="path to output label binarizer")
+ap.add_argument("-e", "--epochs", type=int, default=25,
+                help="# of epochs to train our network for")
+ap.add_argument("-p", "--plot", type=str, default="plot.png",
+                help="path to output loss/accuracy plot")
+args = vars(ap.parse_args())
 
 # initialize the set of labels from the spots activity dataset we are
 # going to train our network on
