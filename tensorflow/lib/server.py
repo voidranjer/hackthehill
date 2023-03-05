826b69e13ca298ea7387f106ca67f986403b4038
meekranjer <logicism81@gmail.com>
2023-03-04 21:56:58 -0500

    Added threshold

--- tensorflow/lib/server.py
+++ tensorflow/lib/server.py
@@ -12,6 +12,8 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
 WEBSOCKETS_PORT = 8765
 CONNECTIONS = set()
 
+CONFIDENCE_THRESH = 0.8
+
 
 async def register(websocket):
     CONNECTIONS.add(websocket)
@@ -56,17 +58,21 @@ async def tensorflow_detection():
 
         image = Image.fromarray(frame)
         results = model.predict(image)
-        highest_confidence = results["predictions"][0]["label"]
+        most_confident_result = results["predictions"][0]
 
-        # Send a message to the client if the highest confidence has changed
-        if highest_confidence != prev_highest_confidence:
-            websockets.broadcast(CONNECTIONS, highest_confidence)
+        print(most_confident_result)
+        highest_label = most_confident_result["label"]
+        highest_confidence = most_confident_result["confidence"]
+
+        # Send a message to the client if the highest confidence has changed (and is above threshold)
+        if (highest_label != prev_highest_label and highest_confidence > CONFIDENCE_THRESH):
+            websockets.broadcast(CONNECTIONS, highest_label)
             print(
-                f"Sent message to {len(CONNECTIONS)} clients: {highest_confidence}")
+                f"Sent message to {len(CONNECTIONS)} clients: {highest_label}")
 
-        prev_highest_confidence = highest_confidence
+        prev_highest_label = highest_label
         # Delay required otherwise websocket server will not receive connections
-        await asyncio.sleep(2)
+        await asyncio.sleep(1)
 
 
 async def main():
