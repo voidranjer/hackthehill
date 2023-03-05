4cef01331870db48efca0fde52e7156b4cc25fd9
meekranjer <logicism81@gmail.com>
2023-03-04 20:57:38 -0500

    Updated asyncio sleep period

--- tensorflow/lib/client.py
+++ tensorflow/lib/client.py
@@ -2,10 +2,13 @@ import asyncio
 import websockets
 
 
-async def recv():
-    async with websockets.connect("ws://localhost:8765") as websocket:
-        # await websocket.send("Hello world!")
-        msg = await websocket.recv()
-        print(msg)
+async def listen():
+    uri = "ws://localhost:8765"
+    async with websockets.connect(uri) as websocket:
+        while True:
+            res = await websocket.recv()
+            print(f"<<< {res}")
+            await asyncio.sleep(1)
 
-asyncio.run(recv())
+if __name__ == "__main__":
+    asyncio.run(listen())
--- tensorflow/lib/server.py
+++ tensorflow/lib/server.py
@@ -9,75 +9,69 @@ import websockets
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
 
 # Store all connected clients in a set
-connected_clients = set()
+WEBSOCKETS_PORT = 8765
+CONNECTIONS = set()
 
 
-async def handle_client(websocket, path):
-    # Add the client to the set of connected clients
-    connected_clients.add(websocket)
+async def register(websocket):
+    CONNECTIONS.add(websocket)
     print(
-        f"Client connected, {len(connected_clients)} client(s) currently connected")
+        f"Client connected, {len(CONNECTIONS)} client(s) currently connected")
     try:
-        # Handle incoming messages from the client
-        async for message in websocket:
-            print(f"Received message from client: {message}")
+        await websocket.wait_closed()
     finally:
-        # Remove the client from the set of connected clients when they disconnect
-        connected_clients.remove(websocket)
+        CONNECTIONS.remove(websocket)
         print(
-            f"Client disconnected, {len(connected_clients)} client(s) currently connected")
+            f"Client disconnected, {len(CONNECTIONS)} client(s) currently connected")
 
 
-async def send_message(message):
-    # Send a message to all connected clients
-    for client in connected_clients:
-        await client.send(message)
-    print(f"Sent message to {len(connected_clients)} client(s): {message}")
-
-
-async def main():
+async def tensorflow_detection():
     # Load TensorFlow model
     dir_path = os.getcwd()
     model = TFModel(dir_path=dir_path)
 
-    # Set up the WebSocket server
-    async with websockets.serve(handle_client, "localhost", 8765):
-        print("WebSocket server started")
+    # Capture webcam footage
+    key = cv2. waitKey(1)
+    webcam = cv2.VideoCapture(0)
 
-        # Capture webcam footage
-        key = cv2. waitKey(1)
-        webcam = cv2.VideoCapture(0)
+    prev_highest_confidence = None
+    while True:
+        try:
+            check, frame = webcam.read()
+            # print(check) #prints true as long as the webcam is running
+            # print(frame) #prints matrix values of each framecd
+            cv2.imshow("Capturing", frame)
+            key = cv2.waitKey(1)
 
-        prev_highest_confidence = None
-        while True:
-            try:
-                check, frame = webcam.read()
-                # print(check) #prints true as long as the webcam is running
-                # print(frame) #prints matrix values of each framecd
-                cv2.imshow("Capturing", frame)
-                key = cv2.waitKey(1)
-
-                # break
-                if key == ord('q'):
-                    webcam.release()
-                    cv2.destroyAllWindows()
-                    break
-
-            except(KeyboardInterrupt):
+            # break
+            if key == ord('q'):
                 webcam.release()
                 cv2.destroyAllWindows()
                 break
 
-            image = Image.fromarray(frame)
-            results = model.predict(image)
-            highest_confidence = results["predictions"][0]["label"]
+        except(KeyboardInterrupt):
+            webcam.release()
+            cv2.destroyAllWindows()
+            break
 
-            # Send a message to the client if the highest confidence has changed
-            if highest_confidence != prev_highest_confidence:
-                await send_message(highest_confidence)
+        image = Image.fromarray(frame)
+        results = model.predict(image)
+        highest_confidence = results["predictions"][0]["label"]
 
-            prev_highest_confidence = highest_confidence
+        # Send a message to the client if the highest confidence has changed
+        if highest_confidence != prev_highest_confidence:
+            websockets.broadcast(CONNECTIONS, highest_confidence)
+            print(
+                f"Sent message to {len(CONNECTIONS)} clients: {highest_confidence}")
 
+        prev_highest_confidence = highest_confidence
+        # Delay required otherwise websocket server will not receive connections
+        await asyncio.sleep(2)
+
+
+async def main():
+    async with websockets.serve(register, "localhost", WEBSOCKETS_PORT):
+        await tensorflow_detection()
 
 if __name__ == "__main__":
     asyncio.run(main())
