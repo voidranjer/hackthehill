6bf3c5b84a3b2b2a86a65377530ddfc786452d48
meekranjer <logicism81@gmail.com>
2023-05-20 21:21:37 +0800

    Update camera.py

--- training/camera.py
+++ training/camera.py
@@ -1,21 +1,23 @@
 import cv2
-import time
 
 # Open the default camera
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 # Define a counter for naming the image files
 count = 0
-
-# Start the timer
-start_time = time.time()
 
 # Start capturing and saving images every 1 second
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
     cv2.imshow("Capturing", frame)
-    key = cv2.waitKey(1)
+    key = cv2.waitKey(50)
+
+    shouldCapture = False
+    if key == ord('z'):
+        break
+    elif key == ord(" "):
+        shouldCapture = True
 
     # If frame is read correctly, save to file
     if ret:
@@ -23,17 +25,9 @@ while True:
         filename = f"images/image_{count}.jpg"
 
         # Save the frame as a JPEG image
-        cv2.imwrite(filename, frame)
-
-        # Increment the counter
-        count += 1
-
-    # Wait for 1 second before capturing the next frame
-    time.sleep(0.2)
-
-    # Stop capturing after 5 seconds
-    if time.time() - start_time > 5000:
-        break
+        if shouldCapture:
+            cv2.imwrite(filename, frame)
+            count += 1
 
 # Release the camera
 cap.release()
